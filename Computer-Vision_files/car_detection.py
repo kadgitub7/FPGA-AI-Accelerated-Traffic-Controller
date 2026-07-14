65199a51f2443263bd27f74e9757084ba79fec30
kadgitub7 <kadhir.ponnambalam@gmail.com>
2026-07-14 14:16:02 -0400

    added lane detection
    now based on where the car is we can add and keep track of how many cars are in the lane
    
    The next step is to determine the pixel boxes of the lanes

--- Computer-Vision_files/car_detection.py
+++ Computer-Vision_files/car_detection.py
@@ -6,6 +6,13 @@ model = YOLO('yolov8n.pt') # replace 'n' with 's','m','l', or 'x' for different 
 
 cap = cv.VideoCapture(0)
 
+lane1_box = [0,30,50,80]
+lane2_box = [20,30,70,80]
+lane3_box = [40,30,90,80]
+lane4_box = [60,30,110,80]
+
+lane_count_total = [0,0,0,0]
+
 while True:
     ret, frame = cap.read()
     if not ret:
@@ -15,6 +22,7 @@ while True:
 
     for result in results:
         boxes = result.boxes
+        lane_count_current = [0,0,0,0]
         for box in boxes:
             bounding_box = box.xyxy[0].cpu().numpy()  # Get bounding box coordinates
             print(bounding_box)
@@ -22,8 +30,26 @@ while True:
 
             class_id = int(box.cls[0])
             object = model.names[class_id]
-            print(object, confidence)
-
+            if object == 'car':
+                car_locationX = (bounding_box[0] + bounding_box[2]) / 2
+                car_locationY = (bounding_box[1] + bounding_box[3]) / 2
+                if car_locationX > lane1_box[0] and car_locationX < lane1_box[2] and car_locationY > lane1_box[1] and car_locationY < lane1_box[3]:
+                    print("Car detected in Lane 1")
+                    lane_count_current[0] += 1
+                elif car_locationX > lane2_box[0] and car_locationX < lane2_box[2] and car_locationY > lane2_box[1] and car_locationY < lane2_box[3]:
+                    print("Car detected in Lane 2")
+                    lane_count_current[1] += 1
+                elif car_locationX > lane3_box[0] and car_locationX < lane3_box[2] and car_locationY > lane3_box[1] and car_locationY < lane3_box[3]:
+                    print("Car detected in Lane 3")
+                    lane_count_current[2] += 1
+                elif car_locationX > lane4_box[0] and car_locationX < lane4_box[2] and car_locationY > lane4_box[1] and car_locationY < lane4_box[3]:
+                    print("Car detected in Lane 4")
+                    lane_count_current[3] += 1
+                else:
+                    print("Car detected in no designated lane")
+                print(object, confidence)
+            lane_count_total = lane_count_current
+            
     image = results[0].plot()
     cv.imshow('results', image)
 
