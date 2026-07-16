fe7305303429afcd27b59ae78a24684026f9542c
kadgitub7 <kadhir.ponnambalam@gmail.com>
2026-07-16 14:46:08 -0400

    finalized initial design

--- FPGA_files/traffic_light.v
+++ FPGA_files/traffic_light.v
@@ -10,6 +10,7 @@ module traffic_light(
 );
 
     reg last_light_1, last_light_2;
+    reg [29:0]counter;
 
     localparam [2:0] IDLE = 3'b000,
                      SIGN_1_GREEN = 3'b001,
@@ -21,69 +22,83 @@ module traffic_light(
     reg [2:0] current_state;
     reg [2:0] next_state;
 
-    always @(posedge clk) begin
+    always@(posedge clk) begin
         if (reset) begin
             current_state <= IDLE;
         end else begin
             current_state <= next_state;
         end
-    end
 
-    always@(*) begin
+        light_1 <= last_light_1;
+        light_2 <= last_light_2;
         case (current_state)
             IDLE: begin
-                light_1 = last_light_1;
-                light_2 = last_light_2;
                 if (lane1 + lane2 + lane3 + lane4 == 0) begin
-                    next_state = IDLE;
+                    next_state <= IDLE;
                 end else if (lane1 + lane3 > 0 && lane2 + lane4 == 0) begin
-                    next_state = SIGN_1_GREEN;
+                    next_state <= SIGN_1_GREEN;
                 end else if (lane2 + lane4 > 0 && lane1 + lane3 == 0) begin
-                    next_state = SIGN_1_RED;
+                    next_state <= SIGN_1_RED;
                 end else if (lane1 + lane3 > lane2 + lane4) begin
-                    next_state = SIGN_1_GREEN;
+                    next_state <= SIGN_1_GREEN;
                 end else if (lane2 + lane4 > lane1 + lane3) begin
-                    next_state = SIGN_1_RED;
+                    next_state <= SIGN_1_RED;
                 end else begin
                     if (last_light_1 == 2'b01) begin
-                        next_state = SIGN_1_RED;
+                        next_state <= SIGN_1_RED;
                     end else begin
-                        next_state = SIGN_1_GREEN;
+                        next_state <= SIGN_1_GREEN;
                     end
                 end
             end
 
             SIGN_1_GREEN: begin
                 if(last_light_1 == 1'b1)begin
-                    next_state = IDLE;
+                    next_state <= IDLE;
                 end else begin
-                    next_state = YELLOW;
+                    next_state <= YELLOW;
                 end
             end
 
             SIGN_1_RED: begin
                 if(last_light_1 == 1'b0)begin
-                    next_state = IDLE;
+                    next_state <= IDLE;
                 end else begin
-                    next_state = YELLOW;
+                    next_state <= YELLOW;
                 end
             end
 
             YELLOW: begin
                 if(last_light_1 == 1'b0) begin
                     //wait 3 seconds
-                    light_2 = 2'b01;
-                    next_state = WAIT_3;
+                    light_2 <= 2'b01;
+                    next_state <= WAIT_3;
                 end else begin
-                    light_1 = 2'b01;
-                    next_state = WAIT_3;
+                    light_1 <= 2'b01;
+                    next_state <= WAIT_3;
                 end
             end
 
             WAIT_3: begin
-                // counter for 3 seconds based on 100MHz clock
-                // 100MHz clock has a period of 10ns, so 3 seconds is 3 * 10^9 ns / 10 ns = 300,000,000 clock cycles
-                // Implement a counter to wait for 300,000,000 clock cycles     
+                if (counter == 29'd300000000) begin
+                    counter <= 0;
+                    next_state <= ALL_RED;
+                end
+                else begin
+                    counter <= counter + 1;
+                    next_state <= WAIT_3;
+                end   
+            end
+
+            ALL_RED: begin
+                if(last_light_1 == 2'b01) begin
+                    light_1 <= 2'b00;
+                    light_2 <= 2'b10;
+                end else begin
+                    light_1 <= 2'b10;
+                    light_2 <= 2'b00;
+                end
+                next_state <= IDLE;
             end
 
         endcase
